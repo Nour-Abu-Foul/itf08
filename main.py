2f564b7e63fa75da3def57cdce6bd9878f68ab23
Nour-Abu-Foul <nourabufoul@gmail.com>
2023-07-31 16:13:22 +0300

    new commit

--- main.py
+++ main.py
@@ -4,23 +4,22 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+#def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+   # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#if __name__ == '__main__':
+   # print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 print ("Nour")
-print ("Hi")
 print ("Hello")
 name = input ("Enter your name")
 age = input("Enter your age")
-birthday = input("Enter your Birthday")
-print("name")
-print("age")
-print("birthday")
+dob = input("Enter your Birthday")
+print ("name")
+print ("age")
+print ("dob")
